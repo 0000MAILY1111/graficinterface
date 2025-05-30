a43cf5058955d6ab4ac170d2702c6172c1085ef7
0000MAILY1111 <valdezmelizza19@gmail.com>
2025-05-30 02:38:16 -0400

    signup

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,27 @@
+export default function SignUpPage() {
+    return (
+        <>
+           <div className="flex min-h-screen flex-col items-center justify-center bg-white ">
+              <div className="w-full max-w-sm space-y-6 ">
+                <h1 className="text-center text-2xl font-semibold text-gray-900 ">Sign Up </h1>
+              </div>
+                <form className="space-x-4 "> 
+                    <div className="relative h-fit">
+                        <input className=" w-full rounded-md border border-gray-300  text-sm px-3 pb-1 pt-7 focus:border-black focus-online:border-black focus:ring-0"
+                        type="email" name="email" required/>
+                        <label className="absolute left-3 top-2 text-[12px]" > Email: </label>
+                    </div>
+                     <div className="relative h-fit">
+                        <input className=" w-full rounded-md border border-gray-300  text-sm px-3 pb-1 pt-7 focus:border-black focus-online:border-black focus:ring-0"
+                        type="password" name="password" required minLength={8}/>
+                        <label className="absolute left-3 top-2 text-[12px]" > Password: </label>
+                    </div>
+                    <button className="w-full rounded-md bg-black text-sm text-white  py-2 font-medium hover:bg-gray-500 focus:outline-none disable:cursor-not-allowed disabled:bg-gray-300  ">
+                        Register 
+                    </button>
+
+                </form> 
+           </div>
+        </>
+    )
+}
