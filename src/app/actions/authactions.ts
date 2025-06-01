89c4b04de8aa0c914204b7b857ccf20fe1127a6f
0000MAILY1111 <valdezmelizza19@gmail.com>
2025-06-01 15:31:19 -0400

    onethirty

--- src/app/actions/authactions.ts
+++ src/app/actions/authactions.ts
@@ -1,36 +1,59 @@
 "use server"
 
 import bcrypt from "bcryptjs";
+import { AuthError } from "next-auth";
 import { redirect } from "next/navigation";
 import { signUpSchema } from "y/schemas";
+import { signIn } from "y/server/auth";
 import { db } from "y/server/db";
-import { ZodError } from "zod";
 import { email } from "zod/v4"
+import { ZodError } from "zod";
 
 ///authactions ///siguPshcema register 
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn("credentials", formData)
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case "CredentialsSignin":
+                return "Invalid credentials";
+                default:
+                    return " Something went wrong "
+            }
+        }
+        throw error; // Re-throw the error if it's not an AuthError
+    }
+}
+
 export async function register(
     prevState: string | undefined,
     formData: FormData,
 ) {
     try {
         const { email, password } = await signUpSchema.parseAsync({
-            email: formData.get("email") ,
+            email: formData.get("email"),
             password: formData.get("password"),
         });
         // Dynamic import to avoid bundling db on client
-        const { db } = await import("y/server/db");
-        const existingUser = await db.user.findUnique({
-            where: { email },
+        //const { db } = await import("y/server/db");
+        const user = await db.user.findUnique({
+            where: {
+                email: email,
+            }
         });
 
-        if (existingUser) {
+        if (user) {
             return "User already exists";
         }
         const hash = await bcrypt.hash(password, 10);
 
         await db.user.create({
             data: {
-                email,
+                email: email,
                 password: hash,
             },
         });
@@ -39,8 +62,6 @@ export async function register(
         if (error instanceof ZodError) {
             return error.errors.map((error) => error.message).join(", ");
         }
-        return "Registration failed";
     }
-
     redirect("/signin");
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -21,17 +21,17 @@ export default function Page() {
                     </div>
                      <div className="relative h-fit">
                         <input className=" w-full rounded-md border border-gray-300  text-sm px-3 pb-1 pt-7 focus:border-black focus-online:border-black focus:ring-0"
-                        //type="password" 
+                        type="password" 
                         name="password"
-                        // required minLength={8}
+                        required minLength={8}
                         />
                         <label className="absolute left-3 top-2 text-[12px]" > Password: </label>
                     </div>
                     <button disabled={isPending} className="w-full rounded-md bg-black text-sm text-white  py-2 font-medium hover:bg-gray-500 focus:outline-none disable:cursor-not-allowed disabled:bg-gray-300  ">
-                        { isPending ? "Registering..." : "Register" }
+                        { isPending ? "Registering..." : "Register"}
                     </button>
                     <p className="text-center text-xs text-gray-600 ">Have an account? 
-                        <Link className = "text-blue-400 hover:text-blue-600" href="/signin"> Register </Link>
+                        <Link className = "text-blue-400 hover:text-blue-600" href="/signin"> Sign in </Link>
                     </p>
 
                     {errorMessage && (<p className="text-red-500 text-sm text-center">{errorMessage}</p>)}
